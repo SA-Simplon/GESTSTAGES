06f35909a81d8775549ba174d0ed1b6a51900e2f
SA-Simplon <samrani.ext@simplonformations.onmicrosoft.com>
2024-06-13 15:56:41 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -4,7 +4,7 @@ function connexionBDD()
 {
 	try
 	{
-            $bdd = new PDO('mysql:host=servbd;port=3306;dbname=bdd_geststages;charset=utf8', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $bdd = new PDO('mysql:host=servbd;port=3306;sofiane-bdd-geststages;charset=utf8', 'sofiane', 'Simplon2024', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             return $bdd;
 	}
 	catch(Exception $e)
