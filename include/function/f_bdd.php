86d9a082992eb492bf0abba7cf687af84fc1270e
SA-Simplon <samrani.ext@simplonformations.onmicrosoft.com>
2024-06-14 10:56:30 +0200

    Update include  f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -1,17 +1,29 @@
-﻿<?php
+<?php
 
 function connexionBDD()
 {
-	try
-	{
-            $bdd = new PDO('mysql:host=servbd;port=3306;sofiane-bdd-geststages;charset=utf8', 'sofiane', 'Simplon2024', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    try
+    {
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem' // Chemin vers le certificat RootCA de Azure
+            );
+    
+            $bdd = new PDO(
+            'mysql:host=serv-db-geststages.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8',
+            'colette',
+            'P@ssw0rd',
+            $options
+        );
+            //array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        
             return $bdd;
-	}
-	catch(Exception $e)
-	{
-		$pdo_error = $e->getMessage();
+    }
+    catch(Exception $e)
+    {
+        $pdo_error = $e->getMessage();
                 return false;
-	}
+    }
     
 }
 
